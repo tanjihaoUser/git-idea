fcb7a9187e457dca4459c5301c4de4c5ba98ac91
tanjihao <www111501com@163.com>
2023-09-23 17:09:04 +0800

    first version

---
+++ src/main/java/com/bupt/GitTest.java
@@ -0,0 +1,12 @@
+package com.bupt;
+
+/**
+ * @author 谭继好
+ * @Description
+ * @date 2023-09-23 17:04
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello, git");
+    }
+}
